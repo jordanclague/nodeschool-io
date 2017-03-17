bc35923e4bceec6df809c36f0970ddb534d7b3bc
Jordan Clague <jordanc@deeson.co.uk>
2017-03-17 15:37:25 +0000

    Learnyounode: 2/13

--- core/learnyounode/program.js
+++ core/learnyounode/program.js
@@ -1,1 +1,8 @@
-console.log('HELLO WORLD');
+var numbers = process.argv.slice(2);
+
+var total = 0;
+for (i = 0; i < numbers.length; i++) {
+  total = total + parseInt(numbers[i]);
+}
+
+console.log(total);
