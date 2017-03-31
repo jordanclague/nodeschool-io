ea5aee07ec761176eb0ff9dcbad4f3d56c2e606c
Jordan Clague <jordanc@deeson.co.uk>
2017-03-31 08:53:01 +0100

    Learnyounode: 9/13

--- core/learnyounode/program.js
+++ core/learnyounode/program.js
@@ -1,14 +1,32 @@
 var http = require('http');
 var bl = require('bl');
+var urls = [process.argv[2], process.argv[3], process.argv[4]];
+var results = [];
+var count = 0;
 
-http.get(process.argv[2], function (response) {
-  response.pipe(bl(function (err, data) {
-    if (err) {
-      console.error;
-    }
-    
-    data = data.toString();
-    console.log(data.length);
-    console.log(data);
-  }));
-});
+function printResults() {
+  results.forEach(function(result) {
+    console.log(result);
+  });
+}
+
+function httpGet(i) {
+  http.get(urls[i], function(response) {
+    response.pipe(bl(function(err, data) {
+      if (err) {
+        console.error(err);
+      }
+
+      results[i] = data.toString();
+      count++;
+
+      if (count == 3) {
+        printResults();
+      }
+    }));
+  });
+}
+
+for (var i = 0; i<3; i++) {
+  httpGet(i);
+}
