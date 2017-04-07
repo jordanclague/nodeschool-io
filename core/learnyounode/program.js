0ef53dcd8fd064f24a6b387a1e6fb667f6b43756
Jordan Clague <jordanc@deeson.co.uk>
2017-04-07 08:21:12 +0100

    Learnyounode: 11/13

--- core/learnyounode/program.js
+++ core/learnyounode/program.js
@@ -1,14 +1,8 @@
-var net = require('net');
+var http = require('http');
+var fs = require('fs');
 
-var server = net.createServer(function (socket) {
-  var d = new Date();
-  var year = d.getFullYear();
-  var month = ("0" + (d.getMonth() + 1)).slice(-2);
-  var date = ("0" + d.getDate()).slice(-2);
-  var hour = d.getHours();
-  var minutes = ("0" + d.getMinutes()).slice(-2); 
-
-  var dateString = year + "-" + month + "-" + date + " " + hour + ":" + minutes;
-  socket.end(dateString + '\n');
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type':'text/plain'});
+  fs.createReadStream(process.argv[3]).pipe(response);
 });
-server.listen(Number(process.argv[2]));
+server.listen(process.argv[2]);
