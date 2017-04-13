8cea8ff9ca965ff0e793728a56f3c52233016136
Jordan Clague <jordanc@deeson.co.uk>
2017-04-13 16:35:05 +0100

    Learnyounode: 13/13

--- core/learnyounode/program.js
+++ core/learnyounode/program.js
@@ -1,9 +1,34 @@
 var http = require('http');
+var url = require('url');
 var map = require('through2-map');
 
-var server = http.createServer(function (request, response) {
-  request.pipe(map(function (chunk) {
-    return chunk.toString().toUpperCase();
-  })).pipe(response);
+function parseTime(iso) {
+  var date = new Date(iso);
+  return JSON.stringify({
+    "hour": date.getHours(),
+    "minute": date.getMinutes(),
+    "second": date.getSeconds()
+  });
+}
+
+function unixTime(iso) {
+  var date = new Date(iso);
+  return JSON.stringify({
+    "unixtime": date.getTime()
+  });
+}
+
+var server = http.createServer(function (request, response) { 
+  var parsedUrl = url.parse(request.url, true);
+  if (parsedUrl.pathname.match(/parsetime$/)) {
+    response.writeHead(200, { 'Content-Type':'application/json' });
+    response.write(parseTime(parsedUrl.query.iso));
+    response.end();
+  }
+  if (parsedUrl.pathname.match(/unixtime$/)) {
+    response.writeHead(200, { 'Content-Type':'application/json' });
+    response.write(unixTime(parsedUrl.query.iso));
+    response.end();
+  }
 });
 server.listen(process.argv[2]);
