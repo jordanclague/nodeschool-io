f4e5ecc291698ec2117c4588bc0edd421a85899c
Jordan Clague <jordanc@deeson.co.uk>
2017-04-13 15:35:57 +0100

    Learnyounode 12/13

--- core/learnyounode/program.js
+++ core/learnyounode/program.js
@@ -1,8 +1,9 @@
 var http = require('http');
-var fs = require('fs');
+var map = require('through2-map');
 
 var server = http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type':'text/plain'});
-  fs.createReadStream(process.argv[3]).pipe(response);
+  request.pipe(map(function (chunk) {
+    return chunk.toString().toUpperCase();
+  })).pipe(response);
 });
 server.listen(process.argv[2]);
